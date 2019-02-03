4720a2842093e0f550daf93c258de445840f8f0b
vosem <trefoil2@ukr.net>
2019-02-03 14:22:05 +0200

    exploring services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -21,7 +21,8 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   };
 
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
-  };
+getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+}
 }
