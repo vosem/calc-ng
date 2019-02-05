1438c5079f31d69b0d7a6899ca1f5440e8f3a179
vosem <trefoil2@ukr.net>
2019-02-05 14:45:26 +0200

    added server and db

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require("express");
+// var bodyParser = require("body-parser");
+const path = require('path');
+var mongodb = require("mongodb");
+var mongoXlsx = require('mongo-xlsx');
+var fs = require('fs');
+
+const app = express();
+let db,
+	  collection,
+	  dataIns = [];
+
+app.use(express.static(path.join(__dirname, './dist/calc-ng')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + './dist/calc-ng/index.html'));
+});
+app.listen(process.env.PORT || 8080);
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  db = client.db();
+  collection = db.collection('ladies-insurance');
+  saveData();
+});
+
+function saveData (db) {
+  	for(let i of dataIns){
+		console.log(i);
+		collection.insertOne(i);
+	}
+};
+
+function convert() {
+  mongoXlsx.xlsx2MongoData("./dist/calc-ng/assets/Ladies.xlsx", null, function(err, mongoData) {
+    dataIns = mongoData;
+      var json = JSON.stringify(mongoData); //convert it back to json
+      fs.writeFileSync('./dist/calc-ng/assets/test.json', json);
+  });
+};
+convert();
